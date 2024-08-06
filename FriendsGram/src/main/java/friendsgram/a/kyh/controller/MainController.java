df0c49703245faff5caec9f1a807ec9686df1d98
AORUS <AORUS@DESKTOP-1R963E0>
2024-08-06 15:34:08 +0900

    test main

--- FriendsGram/src/main/java/friendsgram/a/kyh/controller/MainController.java
+++ FriendsGram/src/main/java/friendsgram/a/kyh/controller/MainController.java
@@ -1,8 +1,14 @@
 package friendsgram.a.kyh.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	
+	@GetMapping("/main")
+	public String board01() {
+		return "/main/kyh/main";
+	}
  
 }
